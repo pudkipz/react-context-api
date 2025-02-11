2094d76f4870b4e7191ef9dad7f28783a7fec020
Matilda Sönnergaard <matilda.sonnergaard@se.experis.com>
2025-02-11 10:43:27 +0100

    Convert to 2 spaces everywhere

--- src/App.jsx
+++ src/App.jsx
@@ -1,27 +1,29 @@
-import { useEffect, useState } from 'react'
+import { createContext, useEffect, useState } from 'react'
 import Header from './components/Header'
 import Tweets from './components/Tweets'
 import RightSide from './components/RightSide'
 import defaultTweets from './assets/data/tweets.js'
 import user from './assets/data/user.js'
 
+const AppContext = createContext()
+
 function App() {
-    const [tweets, setTweets] = useState(defaultTweets)
-    const [theme, setTheme] = useState('light');
+  const [tweets, setTweets] = useState(defaultTweets)
+  const [theme, setTheme] = useState('light');
 
-    useEffect(() => {
-        theme === 'light'
-          ? document.body.style.backgroundColor = 'white'
-          : document.body.style.backgroundColor = 'black'
-    }, [theme])
+  useEffect(() => {
+    theme === 'light'
+    ? document.body.style.backgroundColor = 'white'
+    : document.body.style.backgroundColor = 'black'
+  }, [theme])
 
-    return (
-        <div className="container">
-            <Header user={user} theme={theme} setTheme={setTheme} />
-            <Tweets tweets={tweets} setTweets={setTweets} user={user} theme={theme}  />
-            <RightSide theme={theme} />
-        </div>
-    )
+  return (
+    <div className="container">
+      <Header user={user} theme={theme} setTheme={setTheme} />
+      <Tweets tweets={tweets} setTweets={setTweets} user={user} theme={theme}  />
+      <RightSide theme={theme} />
+    </div>
+  )
 }
 
-export { App };
+export { App, AppContext };
--- src/components/Tweets.jsx
+++ src/components/Tweets.jsx
@@ -1,20 +1,22 @@
 import CreateTweet from './CreateTweet'
 import Tweet from './Tweet'
 
+
+
 export default function Tweets({ tweets, setTweets, user, theme }) {
   return (
-        <main>
-            <div className={theme === 'dark' ? 'top-bar dark' : 'top-bar'}>
-                <h2 className="title">Home</h2>
-            </div>
+    <main>
+      <div className={theme === 'dark' ? 'top-bar dark' : 'top-bar'}>
+        <h2 className="title">Home</h2>
+      </div>
 
-            <CreateTweet tweets={tweets} setTweets={setTweets} user={user} theme={theme} />
+      <CreateTweet tweets={tweets} setTweets={setTweets} user={user} theme={theme} />
 
-            <div className="show-more-tweets">
-                <p>Show 35 Tweets</p>
-            </div>
+      <div className="show-more-tweets">
+        <p>Show 35 Tweets</p>
+      </div>
 
-            {tweets.map((tweet, index) => <Tweet tweet={tweet} theme={theme} key={index} />)}
-        </main>
-    )
+      {tweets.map((tweet, index) => <Tweet tweet={tweet} theme={theme} key={index} />)}
+    </main>
+  )
 }
